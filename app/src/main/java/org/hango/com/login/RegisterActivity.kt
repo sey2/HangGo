18593b172b25043b4a2d981a32cbc7a5ea88f6f5
casio2978 <casio2978@naver.com>
2022-08-07 19:27:06 +0900

    [Update] 회원가입 비밀번호 확인코드 수정 (08.07)

--- app/src/main/java/org/hango/com/login/RegisterActivity.kt
+++ app/src/main/java/org/hango/com/login/RegisterActivity.kt
@@ -79,41 +79,73 @@ class RegisterActivity : AppCompatActivity() {
             val userPass = binding.etPass.getText().toString()
             val userName = binding.etName.getText().toString()
             val PassCk = binding.etPassck.getText().toString()
-            val responseListener =
-                Response.Listener<String?> { response ->
 
-                    //volley
-                    try {
-                        val jasonObject = JSONObject(response) //Register2 php에 response
-                        val success = jasonObject.getBoolean("success") //Register2 php에 sucess
-                        if (userPass == PassCk) {
-                            if (success) { //회원등록 성공한 경우
-                                Toast.makeText(applicationContext, "회원 등록 성공", Toast.LENGTH_SHORT)
-                                    .show()
-                                val intent = Intent(
-                                    this@RegisterActivity,
-                                    LoginActivity::class.java
+            if(!validate)
+                Toast.makeText(applicationContext, "아이디 중복 확인을 해주세요", Toast.LENGTH_LONG).show()
+            else if(checkPass(binding)){
+                Log.e("ERROR", "Password 입력 오류")
+            }
+            else {
+                val responseListener =
+                    Response.Listener<String?> { response ->
+
+                        //volley
+                        try {
+                            val jasonObject = JSONObject(response) //Register2 php에 response
+                            val success = jasonObject.getBoolean("success") //Register2 php에 sucess
+                            if (userPass == PassCk) {
+                                if (success) { //회원등록 성공한 경우
+                                    Toast.makeText(
+                                        applicationContext,
+                                        "회원 등록 성공",
+                                        Toast.LENGTH_SHORT
+                                    )
+                                        .show()
+                                    val intent = Intent(
+                                        this@RegisterActivity,
+                                        LoginActivity::class.java
+                                    )
+                                    startActivity(intent)
+                                }
+                            } else { //회원등록 실패한 경우
+                                Toast.makeText(
+                                    applicationContext,
+                                    "비밀번호를 확인해 주세요",
+                                    Toast.LENGTH_SHORT
                                 )
-                                startActivity(intent)
+                                    .show()
+                                return@Listener
                             }
-                        } else { //회원등록 실패한 경우
-                            Toast.makeText(applicationContext, "회원 등록 실패", Toast.LENGTH_SHORT)
-                                .show()
-                            return@Listener
+                        } catch (e: JSONException) {
+                            e.printStackTrace()
                         }
-                    } catch (e: JSONException) {
-                        e.printStackTrace()
                     }
-                }
-            //서버로 volley를 이용해서 요청을 함
-            val registerRequest = RegisterRequest(
-                userID,
-                userPass,
-                userName,
-                responseListener
-            )
-            val queue = Volley.newRequestQueue(this@RegisterActivity)
-            queue.add(registerRequest)
+                //서버로 volley를 이용해서 요청을 함
+                val registerRequest = RegisterRequest(
+                    userID,
+                    userPass,
+                    userName,
+                    responseListener
+                )
+                val queue = Volley.newRequestQueue(this@RegisterActivity)
+                queue.add(registerRequest)
+            }
         })
+    }
+
+    private fun checkPass(binding : ActivityRegisterBinding) : Boolean{
+        val pass = binding.etPass.getText()
+        val passCk = binding.etPassck.getText()
+
+        if(pass.length >=8 && passCk.length >= 8 && pass.equals(passCk)) return true
+        else if(pass.length < 8)
+            Toast.makeText(applicationContext, "비밀번호는 8자 이상이여야 합니다.", Toast.LENGTH_LONG).show()
+        else if(passCk.length < 8)
+            Toast.makeText(applicationContext,"비밀번호 확인칸을 다시 확인해주세요.", Toast.LENGTH_LONG).show()
+         else if(pass.equals(passCk))
+            Toast.makeText(applicationContext, "입력하신 비밀번호와 비밀번호 확인칸이 일치 하지 않습니다.",Toast.LENGTH_LONG).show()
+
+        return false
+
     }
 }
